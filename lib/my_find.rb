7edcf0e7dc077d785ef3c0b5af77b0f9f65718fc
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 15:58:42 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0 
+  while i < collection.size do 
+    if yield(collection[i]) 
+      found = collection[i]
+      break
+    end
+  
+    i += 1
+  end 
+  found
 end
